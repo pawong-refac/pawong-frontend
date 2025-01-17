9eb5f44bef3582196d53f5ef615f1334db31e816
iam-eungin <1618232@donga.ac.kr>
2025-01-17 10:22:11 +0900

    refactor: BrowserRouter 위치 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import DefaultLayout from "./layout/DefaultLayout";
+import Header from "./components/Common/Header/Header";
+import NavBar from "./components/Common/NavBar/NavBar";
+import AppPages from "./route/AppPages";
+import ScrollToTop from "./layout/ScrollToTop";
 
 function App() {
   return (
-    <div
-      id="app"
-      className="flex flex-col justify-center items-center w-full bg-light_gray h-screen"
-    >
-      <DefaultLayout />
-    </div>
+    <BrowserRouter>
+      <div
+        id="app"
+        className="flex flex-col justify-center items-center w-full bg-light_gray h-screen"
+      >
+        <ScrollToTop />
+        <Header />
+        <AppPages />
+        <NavBar />
+      </div>
+    </BrowserRouter>
   );
 }
 
