8438bad3e109d635838b25026c3d56d305c686a7
ummfieg <ummfieg@naver.com>
2025-01-24 03:24:36 +0900

    refactor/#7 css components 수정 및 페이지 컴포넌트 적용

--- src/domains/Diary/pages/AddDiary.jsx
+++ src/domains/Diary/pages/AddDiary.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import profile from "/assets/Diary/dog-default-img.svg";
+import ImgUpload from "../components/ImgUpload";
 
-//TODO input 컴포넌트화 하기 (image / form)
-//TODO 버튼 컴포넌트 수정하기
 function AddDiary() {
   const [formData, setFormData] = useState({
     tag: "",
@@ -65,54 +63,25 @@ function AddDiary() {
     setIsSubmitted(false);
   };
 
+  const handleCancel = () => {
+    setFormData({
+      tag: "",
+      title: "",
+      place: "",
+      story: "",
+    });
+    setImages([]);
+  };
+
   return (
     <div className="bg_container">
       <div className="content_container">
         <form onSubmit={handleSubmit}>
-          <div
-            className={`flex flex-col items-center justify-center h-64 gap-1 mb-3 bg-white border border-gray text-dark_gray
-            `}
-          >
-            <label htmlFor="image-upload">
-              <img
-                src={profile}
-                alt="dogProfile"
-                className="w-16 h-16 cursor-pointer"
-              />
-            </label>
-            {images.length === 0 && (
-              <p className="text-pastel_blue">이미지를 선택해주세요!</p>
-            )}
-            <p className="mb-4">사진은 최대 3장까지 첨부할 수 있어요!</p>
-            <input
-              type="file"
-              accept="image/jpg, image/jpeg, image/png, image/*"
-              multiple
-              onChange={handleImageChange}
-              className="hidden"
-              id="image-upload"
-            />
-            {images.length > 0 && (
-              <div className="flex flex-row gap-2">
-                {images.map((image, index) => (
-                  <div key={index} className="relative">
-                    <img
-                      src={URL.createObjectURL(image)}
-                      alt={`preview-${index}`}
-                      className="object-cover w-24 h-24 border rounded-lg border-gray"
-                    />
-                    <button
-                      type="button"
-                      onClick={() => handleDeleteImage(index)}
-                      className="absolute flex items-center justify-center w-6 h-6 text-white rounded-full top-1 right-1 bg-pastel_blue"
-                    >
-                      X
-                    </button>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
+          <ImgUpload
+            images={images}
+            handleImageChange={handleImageChange}
+            handleDeleteImage={handleDeleteImage}
+          />
           <p className="my-5 text-xl fontBold">임시보호 일지를 작성해주세요</p>
           <div className="flex flex-col gap-2 px-2">
             <label htmlFor="tag" className="mb-1">
@@ -185,7 +154,9 @@ function AddDiary() {
             ></textarea>
           </div>
           <div className="flex justify-between px-5">
-            <button className="cancelBtn">취소</button>
+            <button className="cancelBtn" type="button" onClick={handleCancel}>
+              취소
+            </button>
             <button className="submitBtn" type="submit">
               등록
             </button>
--- src/domains/Diary/pages/DetailDiary.jsx
+++ src/domains/Diary/pages/DetailDiary.jsx
@@ -5,21 +5,51 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faMessage } from "@fortawesome/free-regular-svg-icons";
 import dummyData from "../components/dummyData.js";
+import { useNavigate } from "react-router-dom";
+
+//TODO 이미지 여려장 표시 수정
+//TODO 댓글 구현
 
 function DetailDiary() {
+  const navigate = useNavigate();
+  const handlegoBackClick = () => {
+    navigate(-1);
+  };
+
+  const otherPost = Array.from({ length: 3 }, () => ({
+    tag: ["오산완", "일상", "병원", "먹방", "미용"][
+      Math.floor(Math.random() * 5)
+    ],
+    likes: Math.floor(Math.random() * (500 - 10 + 1)) + 10,
+    title: [
+      "근처공원에 산책 다녀왔어요!",
+      "강아지 물품 나눔해요!",
+      "올만에 미용!! 묭실 추천합니다!!!",
+      "강아지 페스티벌 정보 공유해요",
+      "사료 추천받아요!! 강아지 고양이도 좋아요",
+      "미세먼지 때문에 짤븐 산책",
+      "집에서 어떻게 놀아줘야하나요",
+      "모카가 자꾸 쌩 시금치를 물고 뜯네요",
+    ][Math.floor(Math.random() * 8)],
+  }));
+
   return (
     <>
-      <div className="w-full h-full bg-white">
+      <div className="w-full h-full bg-ligh-gray ">
         <div className="content_container">
           <div className="flex flex-row items-center gap-2">
-            <FontAwesomeIcon icon={faChevronLeft} className="cursor-pointer" />
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              className="h-5 cursor-pointer"
+              onClick={handlegoBackClick}
+            />
             <div className="px-4 py-1 text-center fontBold bg-light_blue text-pastel_blue rounded-2xl w-28">
               {dummyData.tag}
             </div>
             <p className="text-xl fontBold">{dummyData.title}</p>
           </div>
 
-          <div className="flex flex-row items-center gap-2 px-5 my-5 ">
+          <div className="flex flex-row items-center gap-2 px-5 mt-5 ">
             <img
               src={dummyData.profileImg}
               className="rounded-full w-14 h-14"
@@ -29,7 +59,7 @@ function DetailDiary() {
               <p className="text-base text-dark_gray">{dummyData.place}</p>
             </div>
           </div>
-          <div className="flex justify-end gap-2 px-1 mb-1 text-gray">
+          <div className="flex justify-end gap-2 px-2 mb-1 text-gray">
             <button
               className=" cursor-pointe hover:text-pastel_blue"
               type="button"
@@ -48,8 +78,8 @@ function DetailDiary() {
           </div>
           <p className="px-2 my-4">{dummyData.story}</p>
 
-          <div className="flex flex-row items-center">
-            <p className="px-2 text-sm text-dark_gray">{dummyData.date}</p>
+          <div className="flex flex-row items-center px-2">
+            <p className="text-sm text-dark_gray">{dummyData.date}</p>
             <div className="flex items-center gap-2 ml-auto">
               <FontAwesomeIcon
                 icon={faHeart}
@@ -65,61 +95,35 @@ function DetailDiary() {
           </div>
         </div>
         <div className="content_container">
-          <p className="my-3 fontBold">
+          <p className="my-5 fontBold">
             <span className="text-pastel_blue">다른 게시글</span>도 이어보세요
           </p>
           <div className="flex flex-row justify-between">
-            <div className="flex flex-col items-center justify-center gap-2">
-              <img
-                src={"https://placehold.co/175"}
-                alt="Placeholder Image"
-                className="rounded-lg cursor-pointer"
-              />
-              <div className="flex flex-row items-center gap-2 w-44">
-                <div className="px-4 py-1 text-center fontBold bg-light_blue text-pastel_blue rounded-2xl w-28">
-                  #오산완
-                </div>
-                <FontAwesomeIcon
-                  icon={faHeart}
-                  className="w-6 h-6 text-pink-400"
+            {otherPost.map((post, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center gap-2 h-72"
+              >
+                <img
+                  src={"https://placehold.co/175"}
+                  alt="Placeholder Image"
+                  className="rounded-lg cursor-pointer"
                 />
-                922
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-2">
-              <img
-                src={"https://placehold.co/175"}
-                alt="Placeholder Image"
-                className="rounded-lg cursor-pointer"
-              />
-              <div className="flex flex-row items-center gap-2 w-44">
-                <div className="px-4 py-1 text-center fontBold bg-light_blue text-pastel_blue rounded-2xl w-28">
-                  #오산완
+                <div className="flex flex-row items-center gap-2 w-44">
+                  <div className="px-4 py-1 text-center fontBold bg-light_blue text-pastel_blue rounded-2xl w-28">
+                    #{post.tag}
+                  </div>
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className="w-5 h-5 text-pink-400"
+                  />
+                  {post.likes}
                 </div>
-                <FontAwesomeIcon
-                  icon={faHeart}
-                  className="w-6 h-6 text-pink-400 "
-                />
-                922
+                <p className="w-40 overflow-hidden cursor-pointer h-14 text-dark_gray text-ellipsis">
+                  {post.title}
+                </p>
               </div>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-2">
-              <img
-                src={"https://placehold.co/175"}
-                alt="Placeholder Image"
-                className="rounded-lg cursor-pointer"
-              />
-              <div className="flex flex-row items-center gap-2 w-44">
-                <div className="px-4 py-1 text-center fontBold bg-light_blue text-pastel_blue rounded-2xl w-28">
-                  #일상
-                </div>
-                <FontAwesomeIcon
-                  icon={faHeart}
-                  className="w-6 h-6 text-pink-400"
-                />
-                922
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
