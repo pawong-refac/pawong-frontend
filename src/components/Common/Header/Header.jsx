94d0d7a6b8687f3806ffcf8cae5cd34a8f2b6743
iam-eungin <1618232@donga.ac.kr>
2025-01-24 00:46:11 +0900

    fix: App 수정

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -3,10 +3,10 @@ import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
-  if (location.pathname === "/login" || location.pathname === "/admin") return null;
+  if (location.pathname === "/login" || location.pathname === "/loginAdmin") return null;
 
   return (
-    <div className="w-full h-8 p-4 bg-white flex flex-row justify-between items-center">
+    <div className="max-w-[37.5rem] w-full h-8 p-4 bg-white flex flex-row justify-between items-center">
       <p>logo</p>
       <Link to="/login">
         <p>login</p>
