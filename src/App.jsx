1f1c3c539b3e61a3a1992b5e16d112a09bd65999
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2025-01-31 09:57:35 +0900

    fix: 로그인 페이지에서 기본 배경색 보이는 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Common/Header/Header";
 import NavBar from "./components/Common/NavBar/NavBar";
 import AppPages from "./route/AppPages";
 import ScrollToTop from "./hooks/ScrollToTop";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
+    <>
       <ScrollToTop />
       <Header />
       <div
         id="app"
         className="flex flex-col justify-center items-center max-w-[600px] w-full bg-pastel_gray h-full"
       >
-        <div id="pages" className="w-full py-14">
+        <div
+          id="pages"
+          className={
+            location.pathname === ("/login" || "/loginAdmin")
+              ? "w-full"
+              : "w-full py-14"
+          }
+        >
           <AppPages />
         </div>
       </div>
       <NavBar />
-    </BrowserRouter>
+    </>
   );
 }
 
